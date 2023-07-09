686e04983ef3c0ca3ab233bfe682910ea001a391
subhanshu12 <tripathishubhanshu3@gmail.com>
2023-07-09 21:21:21 +0530

    initial comit

---
+++ path module/index.js
@@ -0,0 +1,7 @@
+const path = require("path");
+ const a=path.basename("/foo/bar/baz/asdf/quux.html")
+ const b=path.dirname("/foo/bar/baz/asdf/quux.html")
+ const c=path.extname("/foo/bar/baz/asdf/quux.html")
+ console.log(a);
+ console.log(b);
+ console.log(c);
